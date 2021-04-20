9499a0e75a09e9a5ffb6f9cf3174f42938a9666a
Maxim <maxnes500@gmail.com>
2021-04-20 17:34:54 +0900

    Added About page

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,18 +1,18 @@
 import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap'
 
 const Footer = () => {
-    return (
-        <footer>
-        <Container>
-           <Row>
-               <Col className='text-center py3' >
-                    Copyright &copy; ProShop
-               </Col>
-           </Row>
-            </Container>
-        </footer>
-    )
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className='text-center py3'>
+            Copyright &copy; Maxim Nesterov 2021
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  )
 }
 
 export default Footer
--- frontend/src/screens/AboutScreen.js
+++ frontend/src/screens/AboutScreen.js
@@ -1,11 +1,90 @@
 import React from 'react'
 import Meta from '../components/Meta'
+import { Col, Row } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import { Card } from 'react-bootstrap'
 
-const AboutScreen = () => {
+const AboutScreen = ({}) => {
   return (
     <>
       <Meta title="About Page | Maxim Nesterov's MERN store" />
-      <h1>Hi</h1>
+      <h1 className='about-heading'>FULL MERN Website</h1>
+      <p>
+        This e-commerce website is entirely created by using the following
+        frameworks and libraries:{' '}
+      </p>
+      <p>MongoDB, Express, React, Node</p>
+      <p>
+        Git repository for this website:{' '}
+        <a
+          href='https://github.com/maxnesjp/mern_website'
+          className='about-a-tag'
+        >
+          <i class='fab fa-github-square fa-2x about-icon'></i> MERN Website -
+          maxnesjp
+        </a>
+      </p>
+      <Card className='my-3 p-3 rounded'>
+        <Card.Img src='/images/sample-code-screenshot.png' variant='top' />
+        <Card.Text as='h3' className='about-heading'>
+          Sample Screenshot
+        </Card.Text>
+      </Card>
+      <hr></hr>
+      <h4 className='about-heading'>My other projects</h4>
+      <p>
+        1. Feel free to check out{' '}
+        <a className='about-a-tag' href='https://maxnes-blog.herokuapp.com/'>
+          <i class='fab fa-python fa-2x about-icon'></i> Blog Website
+        </a>{' '}
+        that I created using Python Flask
+        <p>
+          However, at the current moment, Contact Page for the website is turned
+          off
+        </p>
+      </p>
+      <p>
+        2. First ever created{' '}
+        <a
+          className='about-a-tag'
+          href='https://maxnesjp.github.io/my_website/'
+        >
+          <i class='fab fa-html5 fa-2x about-icon'></i> HTML5 + CSS website
+        </a>{' '}
+        (which I am not really proud), but we all started with something simple,
+        right?
+      </p>
+
+      <hr></hr>
+      <h3 className='about-footer-heading'>Maxim Nesterov's social media</h3>
+      <Row className='about-row'>
+        <Col xs={3}>
+          <a href='https://github.com/maxnesjp' className='about-a-tag'>
+            <i class='fab fa-github-square fa-3x about-icon'></i>
+          </a>
+        </Col>
+        <Col xs={3}>
+          <a
+            href='https://www.linkedin.com/in/maxim-nesterov-7b31a9180/'
+            className='about-a-tag'
+          >
+            <i className='fab fa-linkedin fa-3x about-icon'></i>
+          </a>
+        </Col>
+        <Col xs={3}>
+          <a href='https://www.facebook.com/NesMaxJP/' className='about-a-tag'>
+            <i className='fab fa-facebook-square fa-3x about-icon'></i>
+          </a>
+        </Col>
+        <Col xs={3}>
+          <a
+            href='https://www.instagram.com/crazyrussian_jp/'
+            className='about-a-tag'
+          >
+            <i className='fab fa-instagram fa-3x about-icon'></i>
+          </a>
+        </Col>
+      </Row>
     </>
   )
 }
